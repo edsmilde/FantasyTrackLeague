f52dd1bf839dbc325e3b109e463b991a38677d0a
Ed Smilde <edsmilde@gmail.com>
2022-12-24 02:04:39 +0000

    Update auth

---
+++ settings.py
@@ -0,0 +1,6 @@
+import os
+
+APP_SECRET_KEY = os.getenv("APP_SECRET_KEY")
+AUTH0_CLIENT_ID = os.getenv("AUTH0_CLIENT_ID")
+AUTH0_CLIENT_SECRET = os.getenv("AUTH0_CLIENT_SECRET")
+AUTH0_DOMAIN = os.getenv("AUTH0_DOMAIN")
