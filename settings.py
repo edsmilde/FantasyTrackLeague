55cec64302bc6777070ecaf740c0e990fe4d7cb9
Ed Smilde <edsmilde@gmail.com>
2022-12-27 14:00:40 -0800

    Docker + Lightsail

--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
 import os
 
-from data.replit_store import ReplitDataStore
+# from data.replit_store import ReplitDataStore
 
 APP_SECRET_KEY = os.getenv("APP_SECRET_KEY")
 AUTH0_CLIENT_ID = os.getenv("AUTH0_CLIENT_ID")
 AUTH0_CLIENT_SECRET = os.getenv("AUTH0_CLIENT_SECRET")
 AUTH0_DOMAIN = os.getenv("AUTH0_DOMAIN")
 
-data_store = ReplitDataStore()
+# data_store = ReplitDataStore()
