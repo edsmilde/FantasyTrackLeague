f81cbb5d099a72155bb37fec8f699a58bf32a547
Ed Smilde <edsmilde@gmail.com>
2023-01-01 17:39:38 -0800

    Docker scripts

--- settings.py
+++ settings.py
@@ -1,10 +1,12 @@
 import os
 
-# from data.replit_store import ReplitDataStore
 
 APP_SECRET_KEY = os.getenv("APP_SECRET_KEY")
 AUTH0_CLIENT_ID = os.getenv("AUTH0_CLIENT_ID")
 AUTH0_CLIENT_SECRET = os.getenv("AUTH0_CLIENT_SECRET")
 AUTH0_DOMAIN = os.getenv("AUTH0_DOMAIN")
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 
 # data_store = ReplitDataStore()
